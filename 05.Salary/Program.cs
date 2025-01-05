8a515940d33747e8ae12a5ce4768caa18e528cff
mstankin <martistankin@gmail.com>
2025-01-05 18:01:29 +0200

    Salary
    Checks how many tabs are open, depending on what he has open x amount of many is subtracted from his salary

---
+++ 05.Salary/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _05.Salary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int tabCount = int.Parse(Console.ReadLine());
+            int salary = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < tabCount; i++)
+            {
+                string websiteName = Console.ReadLine();
+                switch (websiteName) 
+                {
+                    case "Facebook":
+                        salary -= 150;
+                        break;
+                    case "Instagram":
+                        salary -= 100;
+                        break;
+                    case "Reddit":
+                        salary -= 50;
+                        break;
+                }
+                   
+                if (salary <= 0)
+                {
+                    Console.WriteLine("You have lost your salary.");
+                    return;
+                }
+            }
+        }
+    }
+}
